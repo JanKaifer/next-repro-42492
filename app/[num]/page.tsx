4b66a3f5bd43481325ddb1ada6051b6e83619b56
Jan Kaifer <jan@kaifer.cz>
2023-01-31 11:57:59 +0100

    reduced reproduction

--- app/[num]/page.tsx
+++ app/[num]/page.tsx
@@ -5,12 +5,21 @@ export default function Page({ params: { num } }) {
   return (
     <div>
       {new Array(100).fill(0).map((_, i) => (
-        <div key={i} className={styles.square}>
+        <div
+          key={i}
+          style={{
+            height: 100,
+            width: 100,
+            background: "pink",
+            margin: 10,
+          }}
+        >
           <Link href={`/${Number(num) - 1}`}>lower</Link>
           <div>{num}</div>
           <Link href={`/${Number(num) + 1}`}>higher</Link>
         </div>
       ))}
+      <div className={styles.square} />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,8 @@
 export default function Layout({ children }: any) {
-  const navbarH = 200;
   return (
     <html>
       <head />
-      <body style={{ margin: 0 }}>
-        <div
-          id="header"
-          style={{
-            position: "fixed",
-            height: navbarH,
-            width: "100vw",
-            background: "green",
-          }}
-        ></div>
-        <div
-          id="content"
-          style={{
-            paddingTop: navbarH,
-          }}
-        >
-          {children}
-        </div>
-      </body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
