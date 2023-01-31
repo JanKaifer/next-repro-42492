ea53b3783e15165d2b70c796a3a1733c644a5454
Jan Kaifer <jan@kaifer.cz>
2023-01-31 11:28:17 +0100

    completed repro

--- app/[num]/page.tsx
+++ app/[num]/page.tsx
@@ -1,18 +1,11 @@
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function Page({ params: { num } }) {
   return (
     <div>
       {new Array(100).fill(0).map((_, i) => (
-        <div
-          key={i}
-          style={{
-            height: 100,
-            width: 100,
-            background: "pink",
-            margin: 10,
-          }}
-        >
+        <div key={i} className={styles.square}>
           <Link href={`/${Number(num) - 1}`}>lower</Link>
           <div>{num}</div>
           <Link href={`/${Number(num) + 1}`}>higher</Link>
