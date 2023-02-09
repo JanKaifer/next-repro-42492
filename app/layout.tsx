bbb80104aca209184bc515ab411bb9a62d141bd7
Jan Kaifer <jan@kaifer.cz>
2023-02-09 12:38:16 +0100

    added top message for easier debugging

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ export default function Layout({ children }: any) {
   return (
     <html>
       <head />
-      <body style={{ margin: 0 }}>{children}</body>
+      <body style={{ margin: 0 }}>
+        this is top
+        {children}</body>
     </html>
   );
 }
